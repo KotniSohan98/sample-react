f2cd45bfa6ca4ec7f11e0b038ce12cb09745020a
KotniSohan98 <64541836+KotniSohan98@users.noreply.github.com>
2024-12-24 09:52:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.scss";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="green">Hello CodeSandbox</h1>
+      <h2 className="green">Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
